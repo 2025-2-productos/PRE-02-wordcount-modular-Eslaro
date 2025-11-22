ac620334a027296b0c1dc7ab795ec598ff0625a3
Eslaro <eslaro47@gmail.com>
2025-11-22 10:16:05 -0500

    Cons Magicas

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,26 +1,15 @@
-# obtain a list of files in the input directory
-
-from homework.src._internals.count_words import count_words
-from homework.src._internals.preprocess_lines import preprocess_lines
-from homework.src._internals.read_all_lines import read_all_lines
-from homework.src._internals.split_into_words import split_into_words
-
+from ._internals.count_words import count_words
+from ._internals.preprocess_lines import preprocess_lines
+from ._internals.read_all_lines import read_all_lines
+from ._internals.split_into_words import split_into_words
 from ._internals.write_count_words import write_count_words
 
 
 def main():
-    ## read all lines
     all_lines = read_all_lines()
-
-    ### preprocess lines
     all_lines = preprocess_lines(all_lines)
-
-    ### split in words
     words = split_into_words(all_lines)
-
-    ### count words
     counter = count_words(words)
-
     write_count_words(counter)
 
 
